49d0d41432b9150b1ca84956244687155b15a728
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2019-03-30 13:13:50 -0500

    Add test for p2.1

---
+++ Clase1-RegistrosFijos/p2.cpp
@@ -0,0 +1,25 @@
+#ifndef P1_CPP
+#define P1_CPP
+
+#include <iostream>
+#include <iomanip>
+
+#include "Aula.h"
+#include "Alumno.h"
+
+using namespace std;
+
+int main(){
+    Aula aulita((char*) "./datos.txt");
+
+    for (Alumno& a: aulita.load()){
+        a.print();
+    };
+
+    Alumno nuevoa("Andrea", "Velasquez", "Gushiken", "Computacion", 5, 2000);
+    aulita.add(nuevoa);
+    
+    return EXIT_SUCCESS;
+}
+
+#endif
