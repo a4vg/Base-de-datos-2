1ce3c2544909204e7ebaebbe77953c3ce5b2529c
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2019-03-31 14:35:10 -0500

    Partially implement erase to get last line

--- Clase1-RegistrosFijos/Aula.h
+++ Clase1-RegistrosFijos/Aula.h
@@ -65,5 +65,47 @@ bool Aula::add(Alumno a){
     return true;
 }
 
+bool Aula::erase(int a){
+    string line;
+    fstream data(filename, ios::in | ios::out);
+    if (!data.is_open()) throw runtime_error("Can't open file");
+
+    getline(data, line, '\n');
+    data.seekg (0, ios::end); // get to the end of the file
+
+    int line_length = line.length();
+    int file_length = data.tellg(); 
+    int total_lines = file_length / (line_length);
+
+    cout << "line_length: " << line_length <<endl
+         << "total_lines: " << total_lines <<endl
+         << "file_length: " << file_length << endl;
+
+
+    if (a>total_lines) return false; // register #a doesn't exist
+
+    // get last line
+    data.seekg(file_length-(line_length));
+    if (a!=total_lines) getline(data, line, '\n'); 
+    else line="";
+
+    // delete register #a and move last register (replace)
+    cout << "Last line: " << line <<endl;
+    int beg_line_a = (line_length+1)*(a-1); // (line_length+1) : count the '\n' at the end of each line
+                                   // (a-1): go to the beginning of line #a    
+
+    cout << "Pick line #a from " << beg_line_a << " to " << beg_line_a+line_length <<endl;   
+    data.seekg((line_length+1)*(a-1));
+
+    // data.replace((line_length+1)*(a-1), line_length, "line");
+    data.close();
+
+    return true;
+
+};
+
+
+
+
 
 #endif
