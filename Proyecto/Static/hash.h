93f5d40e0a0bc2a6051208fd41642054ced03dfd
Alessia Yi <alessia.yi@utec.edu.pe>
2019-05-21 00:01:47 -0500

    Static

--- Proyecto/Static/hash.h
+++ Proyecto/Static/hash.h
@@ -12,14 +12,27 @@ class Hash{
 private:
   int d;
   vector <Bucket> table;
-  bool full;
   int fd;
-  int nextBuck=0;
+  int count;
 
 public:
   Hash(int numBuckets, int fdbuck){
     d=numBuckets;
     fd=fdbuck;
+    string countertxt="counter.txt";
+    if (!is_file_exist(countertxt)){
+      ofstream myfile;
+      myfile.open(countertxt);
+      myfile << d-1 << endl;
+      myfile.close();
+    }
+    string line;
+    ifstream myfile;
+    myfile.open(countertxt);
+    getline(myfile, line);
+    count=stoi(line);
+    cout << count << endl;
+    myfile.close();
     for (int i=0; i<d; i++){
       Bucket buck;
       buck.id = i;
@@ -33,7 +46,6 @@ public:
         myfile << i << endl;
         myfile.close();
       }
-      nextBuck++;
     }
   }
 
@@ -55,7 +67,6 @@ public:
       myfile.open(svector);
       while (getline (myfile,line)){
         table[i].overflow.push_back(stoi(line));
-        cout << "line: " << line << endl;
       }
       myfile.close();
 
@@ -71,7 +82,6 @@ public:
     myfile.open(snBucket);
     while (getline (myfile,line)){
       table[buckNum].count+=1;
-      cout << line;
     }
     myfile.close();
     if (table[buckNum].count<fd){
@@ -82,13 +92,19 @@ public:
     }
     else{
       cout <<"Overflow" << endl;
-      nextBuck++;
+      string countertxt="counter.txt";
+      ofstream myfilec;
+      myfilec.open(countertxt);
+      count+=1;
+      myfilec << count << endl;
+      cout << count << endl;
+      myfilec.close();
       Bucket buck;
-      buck.id = nextBuck;
-      buck.name = to_string(nextBuck).append(".txt");
-      buck.overflow.push_back(nextBuck);
+      buck.id = count;
+      buck.name = to_string(count).append(".txt");
+      buck.overflow.push_back(count);
       table.push_back(buck);
-      table[buckHash].overflow.push_back(nextBuck);
+      table[buckHash].overflow.push_back(count);
       string svector="vector" + to_string(buckHash).append(".txt");
       ofstream myfilev;
       myfilev.open(svector, ios::app);
--- Proyecto/Static/main.cpp
+++ Proyecto/Static/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main(){
   Hash myhash(4, 2);
   myhash.read();
-  myhash.insert(24);
+  myhash.insert(32);
   //myhash.insert(12);
   //myhash.print();
 
